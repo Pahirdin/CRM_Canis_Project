c9cbfac0f5c65470eec0255263d1b39cf9db4a39
Ivanna_K <ivanna.kravtsiv@gmail.com>
2023-03-03 19:53:31 -0600

    Feature file is created

--- src/test/java/com/canis/pages/BasePage.java
+++ src/test/java/com/canis/pages/BasePage.java
@@ -3,7 +3,7 @@ package com.canis.pages;
 import com.canis.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
 
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
