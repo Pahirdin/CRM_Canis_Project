4b63796f48b1b436a5ca74668dbbe944e864ee12
An4Chi <kroliktha@gmail.com>
2023-03-07 11:33:21 -0600

    Updated Step Definition

--- src/test/java/com/canis/step_definitions/B28G10_96_TimeAndReport_StepDefinitions.java
+++ src/test/java/com/canis/step_definitions/B28G10_96_TimeAndReport_StepDefinitions.java
@@ -23,14 +23,9 @@ public class B28G10_96_TimeAndReport_StepDefinitions extends TimeAndReportsPage{
     @Then("user should see all modules in Time and Reports page")
     public void user_should_see_all_modules_in_time_and_reports_page() {
         Assert.assertTrue(absenceChartButton.isDisplayed());
-        Assert.assertTrue(absenceChartButton.isEnabled());
         Assert.assertTrue(worktimeButton.isDisplayed());
-        Assert.assertTrue(worktimeButton.isEnabled());
         Assert.assertTrue(bitrix24_TimeButton.isDisplayed());
-        Assert.assertTrue(bitrix24_TimeButton.isEnabled());
         Assert.assertTrue(workReportsButton.isDisplayed());
-        Assert.assertTrue(workReportsButton.isEnabled());
         Assert.assertTrue(meetingsAndBriefingsButton.isDisplayed());
-        Assert.assertTrue(meetingsAndBriefingsButton.isEnabled());
     }
 }
