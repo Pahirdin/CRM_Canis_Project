8102227f9063035405b9abfbde77903e84cb85cd
pahirdin <pahirdin1121@gmail.com>
2023-03-07 00:34:09 -0600

    login/B28G10-98

--- src/test/java/com/canis/runners/CukesRunner.java
+++ src/test/java/com/canis/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/canis/step_definitions",
        dryRun = false,
-        tags = "@configureMenu",
+        tags = "",
         publish = true  // generating a report with public link
 
 
