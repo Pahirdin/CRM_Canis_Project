e4bc3f3bed759b79e8640cd8c9ef7990ccb10940
Pahirdin <114306487+Pahirdin@users.noreply.github.com>
2023-03-09 21:51:04 -0600

    Update B28G10_92_MoreTab_StepDefinitions.java

--- src/test/java/com/canis/step_definitions/B28G10_92_MoreTab_StepDefinitions.java
+++ src/test/java/com/canis/step_definitions/B28G10_92_MoreTab_StepDefinitions.java
@@ -17,16 +17,7 @@ public class B28G10_92_MoreTab_StepDefinitions {
 
     MoreTabPage moreTabPage = new MoreTabPage();
 
-   /* @Given("user is on CRM home page as {string}")
-    public void userIsOnCRMHomePageAs(String arg0) {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-        moreTabPage.userLoginBox.sendKeys("helpdesk1@cybertekschool.com");
-        moreTabPage.userPasswordBox.sendKeys("UserUser");
-        moreTabPage.loginButton.click();
-    }
 
-
-    */
 
     @When("user clicks on more tab")
     public void userClicksOnMoreTab() {
